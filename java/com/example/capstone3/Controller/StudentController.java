c7999f78b57842344d77b5a236cd9a9c7e62b42d
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-08-28 21:09:37 +0300

    Add files via upload

---
+++ java/com/example/capstone3/Controller/StudentController.java
@@ -0,0 +1,129 @@
+package com.example.capstone3.Controller;
+
+import com.example.capstone3.DTO.RequestRideDTO;
+import com.example.capstone3.Model.Compliant;
+import com.example.capstone3.Model.Student;
+import com.example.capstone3.Service.ReviewService;
+import com.example.capstone3.Service.StudentService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/student")
+
+public class StudentController {
+
+    private final StudentService studentService;
+    private final ReviewService reviewService;
+
+    @GetMapping("/get")
+    public ResponseEntity findAllStudents() {
+        return ResponseEntity.status(200).body(studentService.getAllStudents());
+    }
+
+    @PostMapping("/post/{fid}")
+    public ResponseEntity addStudent(@PathVariable int fid , @Valid @RequestBody Student student) {
+        studentService.addStudent(fid,student);
+        return ResponseEntity.status(200).body("student added successfully");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateStudent(@PathVariable int id, @Valid @RequestBody Student student) {
+        studentService.updateStudent(id, student);
+        return ResponseEntity.status(200).body("student updated successfully");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteStudent(@PathVariable int id) {
+        studentService.deleteStudent(id);
+        return ResponseEntity.status(200).body("student deleted successfully");
+    }
+
+//    @PutMapping("/{sid}/subscribe/{cid}/{period}")
+//    public ResponseEntity subscribeStudent(@PathVariable int sid, @PathVariable int cid, @PathVariable @Min(value = 1) @Max(value = 3) int period) {
+//        studentService.subscribeStudent(sid, cid, period);
+//        return ResponseEntity.status(200).body("Student subscribed successfully");
+//    }
+
+    @PutMapping("/{sid}/join/{dtid}")
+    public ResponseEntity joinDailyTrip(@PathVariable int sid, @PathVariable int dtid) {
+        studentService.joinDailyTrip(sid,dtid);
+        return ResponseEntity.status(200).body("Student joined daily trip successfully");
+    }
+
+    @PutMapping("/{sid}/joindeliverygroup/{dgid}")
+    public ResponseEntity joinDeliveryGroup(@PathVariable int sid, @PathVariable int dgid) {
+        studentService.joinFacilityDelivery(sid,dgid);
+        return ResponseEntity.status(200).body("Student joined to delivery group successfully");
+    }
+
+    @PutMapping("/{sid}/review/{cid}/{comment}/{rate}")
+    public ResponseEntity addReview(@PathVariable int sid, @PathVariable int cid, @PathVariable String comment ,  @PathVariable @Min(value = 1) @Max(value = 5) int rate) {
+        studentService.addCaptainReview(sid,cid,comment,rate);
+        return ResponseEntity.status(200).body("Review added successfully");
+    }
+
+
+
+    @GetMapping("/students-report")
+    public ResponseEntity studentReport(){
+        return ResponseEntity.status(200).body(studentService.ReportAllStudent());
+    }
+
+    @GetMapping("/find-student-by-academic-level/{academicLevel}")
+    public ResponseEntity findStudentByAcademicLevel(@PathVariable String academicLevel)
+    {
+        return ResponseEntity.status(200).body(studentService.findStudentByAcademicLevel(academicLevel));
+    }
+    @GetMapping("/find-student-by-gender/{gender}")
+    public ResponseEntity findStudentByGender(@PathVariable String gender)
+    {
+        return ResponseEntity.status(200).body(studentService.findStudentByGender(gender));
+    }
+
+    @PutMapping("/request-ride")
+    public ResponseEntity requestRideStudent(@Valid @RequestBody RequestRideDTO rd) {
+        studentService.requestRide(rd);
+        return ResponseEntity.status(200).body("Student requested successfully");
+    }
+
+
+    @DeleteMapping("/cancel-request/{studentid}")
+    public ResponseEntity cancelRequestStudent(@PathVariable int studentid) {
+        studentService.cancelRequest(studentid);
+        return ResponseEntity.status(200).body("Student request cancelled successfully");
+    }
+
+    @PutMapping("{sid}/changefacility/{fid}")
+    public ResponseEntity changeFacility(@PathVariable int sid, @PathVariable int fid) {
+        studentService.changeFacility(sid,fid);
+        return ResponseEntity.status(200).body("Facility changed successfully");
+    }
+
+
+    @PutMapping("/make-report/{studentid}/{captainid}")
+    public ResponseEntity makeCompliant (@PathVariable int studentid,@PathVariable int captainid ,@Valid @RequestBody Compliant compliant){
+        studentService.makeCompliant(studentid,captainid, compliant);
+        return ResponseEntity.status(200).body("Report make successfully");
+    }
+
+    @GetMapping("/suggestsimilarpath/{studentid}")
+    public ResponseEntity suggestSimilarPathRequests(@PathVariable int studentid){
+        return ResponseEntity.status(200).body(studentService.suggestSimilarPathTrips(studentid));
+    }
+
+    @PostMapping("/add-question/{sid}/{question}")
+    public ResponseEntity addQuestion(@PathVariable Integer sid,@PathVariable String question)
+    {
+        studentService.addQuestion(sid, question);
+        return ResponseEntity.status(200).body(("question added"));
+    }
+
+
+
+}
